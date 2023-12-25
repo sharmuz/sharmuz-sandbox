6915240cfc3f124d39032ca7ccc1e739ebd54fe8
sharmuz <muzher@gmail.com>
2023-12-25 12:26:21 +0000

    Make a tiny change to test git

--- src/simple_fun.py
+++ src/simple_fun.py
@@ -16,9 +16,9 @@ def say_hello(friend: str = "stranger") -> str:
     return f"\nHello there, {friend}!\n"
 
 
-def main():
+def run():
     print(say_hello())
 
 
 if __name__ == "__main__":
-    main()
+    run()
